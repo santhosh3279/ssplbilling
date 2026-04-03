90b8bddd7213849ba1eacf9c731b3098c756a0a2
santhosh3279 <santhosh.sundaran007@gmail.com>
2026-04-03 05:39:37 +0000

    style: increase opacity of UOM prices grid headers in ItemSearch

--- frontend/src/components/ItemSearch.vue
+++ frontend/src/components/ItemSearch.vue
@@ -124,8 +124,8 @@
           <table class="w-full border-collapse">
             <thead>
               <tr class="bg-slate-800/50">
-                <th class="border border-slate-700 px-2 py-1 text-left text-sm font-normal text-slate-500 uppercase w-32">UOM</th>
-                <th v-for="pl in insightData.priceLists" :key="pl.name" class="border border-slate-700 px-2 py-1 text-right text-xl font-normal text-slate-500 uppercase min-w-[120px]">
+                <th class="border border-slate-700 px-2 py-1 text-left text-sm font-normal text-slate-300 uppercase w-32">UOM</th>
+                <th v-for="pl in insightData.priceLists" :key="pl.name" class="border border-slate-700 px-2 py-1 text-right text-xl font-normal text-slate-300 uppercase min-w-[120px]">
                   {{ pl.name }}
                 </th>
               </tr>
