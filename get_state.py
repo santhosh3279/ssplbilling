d00c88daac8f8a9ed094841fcb4a003c482e7dd3
santhosh3279 <santhosh.sundaran007@gmail.com>
2026-04-05 02:28:32 +0000

    chore: cleanup temporary scripts

--- get_state.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-def get_company_state():
-    company = "Sundaram and Sons Private Ltd"
-    address_name = frappe.db.get_value("Dynamic Link", {"link_doctype": "Company", "link_name": company, "parenttype": "Address"}, "parent")
-    if address_name:
-        state = frappe.db.get_value("Address", address_name, "state")
-        return state
-    return None
