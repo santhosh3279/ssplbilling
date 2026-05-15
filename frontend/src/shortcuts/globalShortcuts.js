43a3c1eff0584effa1aa54535fbe7db057040c9b
santhosh3279 <santhosh.sundaran007@gmail.com>
2026-05-15 03:15:11 +0000

    Remove global F4 General Ledger toggle

--- frontend/src/shortcuts/globalShortcuts.js
+++ frontend/src/shortcuts/globalShortcuts.js
@@ -21,9 +21,6 @@ export const globalShortcuts = {
   'F1': () => {
     window.dispatchEvent(new CustomEvent('wb-global-command-line-toggle'));
   },
-  'F4': () => {
-    window.dispatchEvent(new CustomEvent('wb-global-general-ledger-toggle'));
-  },
   'F12': () => {
     window.dispatchEvent(new CustomEvent('wb-global-calculator-toggle'));
   }
