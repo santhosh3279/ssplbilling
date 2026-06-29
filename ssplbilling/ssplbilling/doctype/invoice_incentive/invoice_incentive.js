475bee8e02ed33c54512ea94ecf2c30fd4b33da1
santhosh3279 <santhosh.sundaran007@gmail.com>
2026-06-29 09:44:26 +0000

    Create submittable Invoice Incentive Doctype with child table

---
+++ ssplbilling/ssplbilling/doctype/invoice_incentive/invoice_incentive.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2026, SSPL and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Invoice Incentive", {
+	// refresh: function(frm) {
+	// }
+});
