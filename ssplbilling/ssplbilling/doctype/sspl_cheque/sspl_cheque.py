308b6507ef553e2add2b055fa87b64b217a6da44
santhosh3279 <santhosh.sundaran007@gmail.com>
2026-07-04 06:18:05 +0000

    feat: add amount_in_words field to SSPL Cheque doctype and validate hook to populate it

--- ssplbilling/ssplbilling/doctype/sspl_cheque/sspl_cheque.py
+++ ssplbilling/ssplbilling/doctype/sspl_cheque/sspl_cheque.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2026, SSPL and contributors
 # For license information, please see license.txt
 
+import frappe
 from frappe.model.document import Document
+from frappe.utils import money_in_words
 
 
 class SSPLCheque(Document):
-	pass
+	def validate(self):
+		if self.amount:
+			self.amount_in_words = money_in_words(self.amount, "INR")
+		else:
+			self.amount_in_words = ""
