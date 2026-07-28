7de4541bce4ae7257f994339446a337c75f867a9
santhosh3279 <santhosh.sundaran007@gmail.com>
2026-07-28 16:47:09 +0000

    style(quick-item-search): rate text size 1.25x

--- frontend/src/components/QuickItemSearch.vue
+++ frontend/src/components/QuickItemSearch.vue
@@ -48,7 +48,7 @@
                 </div>
               </div>
               <div class="flex flex-col items-end shrink-0">
-                <div class="text-2xl font-mono font-bold" :class="selectedIndex === itemMeta.globalIndex ? '!text-[var(--color-text-on-focus)]' : 'text-[var(--color-warning)]'">{{ formatPrice(getItemPrice(itemMeta.item)) }}</div>
+                <div class="text-[1.875rem] font-mono font-bold" :class="selectedIndex === itemMeta.globalIndex ? '!text-[var(--color-text-on-focus)]' : 'text-[var(--color-warning)]'">{{ formatPrice(getItemPrice(itemMeta.item)) }}</div>
                 <div 
                   class="text-sm font-bold" 
                   :class="selectedIndex === itemMeta.globalIndex ? '!text-[var(--color-text-on-focus)]' : (itemMeta.item.stock > 20 ? 'text-[var(--color-success)]' : itemMeta.item.stock > 0 ? 'text-[var(--color-warning)]' : 'text-[var(--color-danger)]')"
